5594c8a0f8f8bc56d9437580b96aecacd8c741cb
TODSO <50938440+TODSO@users.noreply.github.com>
2022-02-13 10:33:25 +0900

    Initial commit

---
+++ models/product.js
@@ -0,0 +1,31 @@
+module.exports = function(sequelize, DataTypes) {
+    const product = sequelize.define('Product', {
+        name : {
+            type: DataTypes.STRING(20),
+            allowNull: false
+        },
+        price : {
+            type: DataTypes.INTEGER(10),
+            allowNull: false
+        },
+        seller : {
+            type: DataTypes.STRING(30),
+            allowNull: false
+        },
+        description : {
+            type: DataTypes.STRING(300),
+            allowNull: false
+        },
+        imageUrl: {
+            type: DataTypes.STRING(300),
+            allowNull: true
+        },
+        // 상품이 팔렸는지 안팔렸는지 여부 1과 0으로 
+        soldout : {
+            type: DataTypes.INTEGER(1),
+            allowNull: false,
+            defaultValue: 0,
+        }
+    });
+    return product;
+}
